acdeda3b2b188203119184dfdc78be13c9a7f7aa
Paul Ren <pause.forever@gmail.com>
2022-09-12 22:25:40 +0700

    add exercises

---
+++ ex-1.js
@@ -0,0 +1,26 @@
+const provinces = {
+  Chonburi: 226687,
+  Suphanburi: 41012,
+  Trang: 24826,
+  Trat: 47202,
+  Lopburi: 29815,
+  'Chiang Mai': 48911,
+  Lampang: 12545,
+  Phuket: 88674,
+  Saraburi: 23655,
+  Phrae: 15678,
+  Loei: 76757,
+  'Chiang Rai': 11230,
+  'Nakhon Pathom': 76705,
+  Bangkok: 370177,
+  'Samut Prakan': 38590,
+  Rayong: 40543,
+  Yala: 11315,
+  Ranong: 42612,
+  Phayao: 48147,
+  Khonkaen: 23218,
+  Nan: 37463,
+  'Nakhon Sawan': 74321
+}
+
+// Start coding here
---
+++ ex-3.js
@@ -0,0 +1,16 @@
+const salary = {
+  Mark: 28376,
+  Peter: 41012,
+  Bee: 16079,
+  Jack: 81584,
+  Patrick: 46169,
+  John: 66160,
+  Tee: 15885,
+  Pop: 38590,
+  Lisa: 35786,
+  Alex: 12714,
+  Bank: 15315,
+  Pee: 27477,
+};;
+
+// Start coding here
